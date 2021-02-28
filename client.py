4ad67b21c45b5f22ac13610d5089de50b3f453e0
Paul <pmatthews91@gmail.com>
2021-02-28 18:44:16 +0000

    feat: add client

---
+++ client.py
@@ -0,0 +1,17 @@
+import grpc
+
+from recommendations_pb2 import BookCategory, RecommendationRequest
+from recommendations_pb2_grpc import RecommendationsStub
+
+
+def main() -> None:
+    channel = grpc.insecure_channel("localhost:50051")
+    client = RecommendationsStub(channel)
+    request = RecommendationRequest(
+         user_id=1, category=BookCategory.SCIENCE_FICTION, max_results=3
+    )
+    client.Recommend(request)
+
+
+if __name__ == "__main__":
+    main()
